9588fd6b566f77deeab46577c76f0ce28c9d2277
WojOgi <w.ogieglo@gmail.com>
2022-09-25 06:20:20 +0300

    added a few lines

--- src/main/java/com/kelinci/mainapp/api/HomeController.java
+++ src/main/java/com/kelinci/mainapp/api/HomeController.java
@@ -4,13 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
 @RestController
 public class HomeController {
 
     @GetMapping(value = "/users")
     //czeka na wywolanie localhost:8080/users
-    public User users(){
+    public User users() {
         return new User("Pawel", "Krzak");
     }
 }
