6cac6eb1d238064bb7fbaec247f8e530dcfad025
WojOgi <w.ogieglo@gmail.com>
2022-09-25 06:09:17 +0300

    added a few lines

--- src/main/java/com/kelinci/mainapp/api/HomeController.java
+++ src/main/java/com/kelinci/mainapp/api/HomeController.java
@@ -3,6 +3,7 @@ package com.kelinci.mainapp.api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HomeController {
 
