a46f809cbdfcda151f443bd7e55823d6681ef930
WojOgi <w.ogieglo@gmail.com>
2022-10-11 19:50:37 +0300

    KEL-24 task done by adding int magicNumber which is ignored in register() method.

--- src/main/java/com/kelinci/mainapp/api/Register.java
+++ src/main/java/com/kelinci/mainapp/api/Register.java
@@ -2,9 +2,11 @@ package com.kelinci.mainapp.api;
 
 public class Register {
     private String mail;
+    private int magicNumber;
 
-    public Register(String mail) {
+    public Register(String mail, int magicNumber) {
         this.mail = mail;
+        this.magicNumber = magicNumber;
     }
 
     public String getMail() {
@@ -13,5 +15,13 @@ public class Register {
 
     public void setMail(String mail) {
         this.mail = mail;
+    }
+
+    public int getMagicNumber() {
+        return magicNumber;
+    }
+
+    public void setMagicNumber(int magicNumber) {
+        this.magicNumber = magicNumber;
     }
 }
