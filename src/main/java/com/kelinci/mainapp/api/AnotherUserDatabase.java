9144da8c5099cf67c3ace642a79a6ed2ddf68aea
WojOgi <w.ogieglo@gmail.com>
2022-12-25 14:03:24 +0300

    Testing what happens when more Classes implement UserDatabaseInterface

--- src/main/java/com/kelinci/mainapp/api/AnotherUserDatabase.java
+++ src/main/java/com/kelinci/mainapp/api/AnotherUserDatabase.java
@@ -1,4 +1,36 @@
 package com.kelinci.mainapp.api;
 
-public class AnotherUserDatabase {
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+
+public class AnotherUserDatabase implements UserDatabaseInterface{
+    @Override
+    public List<OurUser> getUserDatabase() {
+        return null;
+    }
+
+    @Override
+    public void addUserToDatabase(OurUser ourUser) {
+
+    }
+
+    @Override
+    public void clearUserDatabase() {
+
+    }
+
+    @Override
+    public Optional<OurUser> getUserWithMail(String mail) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void addRegisteredUser(String mail) {
+
+    }
 }
